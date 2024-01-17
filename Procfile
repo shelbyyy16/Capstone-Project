035fd7e6cc68117936efcb2c00996a54b1146c57
honeybee <shelby.e.pagan@gmail.com>
2024-01-17 12:17:07 -0500

    Config deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn splitexpense.wsgi
--- splitexpense/settings.py
+++ splitexpense/settings.py
@@ -125,3 +125,6 @@ LOGIN_REDIRECT_URL = '/groups/'
 LOGOUT_REDIRECT_URL = '/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
+django_heroku.settings(locals())
